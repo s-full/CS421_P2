5986c095a2f697bafdb630feb86f6f86083a6e02
s-full <stefan.fuller1@gmail.com>
2023-03-02 11:18:11 -0800

    finished shell some bugs with fg

--- myshell.c
+++ myshell.c
@@ -2,8 +2,24 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 #define MAX_LINE 80 /* 80 chars per line, per command, should be enough. */
+
+#define BUFFER_SIZE 50
+
+static char buffer[BUFFER_SIZE];
+pid_t fg_pid = 0;
+
+void handle_SIGQUIT() {
+    write(STDOUT_FILENO,buffer,strlen(buffer));
+    if (fg_pid != 0) {
+        kill(fg_pid, SIGQUIT);
+
+    }
+    //exit(0);
+}
 
 void printWelcome(int currpid) {
 	printf("Welcome to sfshell. My pid is %d. \n", currpid);
@@ -79,56 +95,94 @@ int main(void)
     int background;              /* equals 1 if a command is followed by '&' */
     char *args[(MAX_LINE/2)+1];  /* command line (of 80) has max of 40 arguments */
     int promptCount = 1;
-
+    
+    pid_t pid;
+    
     int pidt = getpid();
+
+    // print introduction/welcome
     printWelcome(pidt);
+
+    // this will handle ctrl+<\> 
+    struct sigaction handler;
+    handler.sa_handler = handle_SIGQUIT;
+    handler.sa_flags = SA_RESTART;
+    sigaction(SIGQUIT, &handler, NULL);
+    strcpy(buffer,"Caught <ctrl><\\>\n");
     
     while (1){            /* Program terminates normally inside setup */
        background = 0;
+
+       // print command promp and count
        printPrompt(&promptCount);
-       setup(inputBuffer,args,&background);       /* get next command */
-        pid_t pid = fork();
+        setup(inputBuffer,args,&background);
+
+        // built in commands
+
+        if (!strcmp("bg", args[0])) {
+            pid_t bg_pid = atoi(args[1]);
+            kill(bg_pid, SIGCONT);
+            fg_pid = 0;
+            continue;
+        }
+        if (!strcmp("stop", args[0])) {
+            pid_t stop_pid = atoi(args[1]);
+            kill(stop_pid, SIGSTOP);
+            continue;
+        }
+        if (!strcmp("fg", args[0])) {
+            pid_t npid = atoi(args[1]);
+            
+            kill(npid, SIGCONT);
+            
+            continue;
+            
+        }
+        if (!strcmp("kill", args[0])) {
+            pid_t kill_pid = atoi(args[1]);
+            kill(kill_pid, SIGKILL);
+            continue;
+
+        } 
+        if (!strcmp("exit", args[0])){
+            break;
+        }
+        
+        pid = fork();
         if (pid < 0) {
             fprintf(stderr, "Fork failed");
             return 1;
         }
         else if (pid == 0) {
-
-           if (!strcmp("bg", args[0])) {
-                execvp("bg", args);
+            printf("[Child pid = %d, background = ", getpid());
+            if (background) {
+                printf("TRUE]\n");
+                //setsid();
             }
-            else if (!strcmp("jobs", args[0])) {
-                execvp("ps", args);
-            }
-            else if (!strcmp("fg", args[0])) {
-                execvp("fg", args);
-            }
-            else if (!strcmp("kill", args[0])) {
-                execvp("kill", args);
-            } 
             else {
-
+                 printf("FALSE]\n");
             }
+            execvp(args[0], args);
         }
         else {
-            waitpid(NULL);
-            printf("child complete\n");
+            if (!background) {
+                fg_pid = pid;
+                waitpid(pid, NULL, WUNTRACED);
+                printf("Child process complete\n");
+                fg_pid = 0;
+            }
+                
+                //waitpid(pid, NULL, 0);
+                
         }
-       
-
       /* the steps are:
        (0) if built-in command, handle internally
        (1) if not, fork a child process using fork()
        (2) the child process will invoke execvp()
        (3) if background == 0, the parent will wait,
-            otherwise returns to the setup() function. */
-       
-    //    if (pid < 0) {
-    //     fprintf(stderr, "Fork Failed");
-    //     return 1; 
-    //    }
-    //    else if (pid == 0) {
-    //    	execvp(args[0], args);
-    //    }            
+            otherwise returns to the setup() function. */       
     }
+    //kill(pid, SIGKILL);
+    printf("sfshell exiting\n");
+    return 0;
 }
